8b85fb273a7bd29e95a29c019b15fdb50c9186c3
Melvin Welton <nivlem65@gmail.com>
2022-02-01 10:09:18 -0600

    commit on main

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-
-
-
+import ContactForm from './components/Contact';
 import './App.css';
 import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div>
-      <Nav/>
+      <Nav />
       <main>
-       
+        <ContactForm></ContactForm>
         <About></About>
         <Gallery></Gallery>
       </main>
-      
+
     </div>
   );
 }
