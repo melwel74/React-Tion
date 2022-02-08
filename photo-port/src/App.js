ba6dd96072651fc9a0a420f6e29e996d520552af
Melvin Welton <nivlem65@gmail.com>
2022-02-07 19:10:48 -0600

    commit on main

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -5,6 +5,10 @@ import About from './components/About';
 import ContactForm from './components/Contact';
 import './App.css';
 import Gallery from './components/Gallery';
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+
+
 
 
 function App(props) {
