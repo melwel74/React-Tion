580886624183b13d97f11a9c1b671b7d0b4b86ab
Melvin Welton <nivlem65@gmail.com>
2022-02-25 07:38:05 -0600

    added project

--- photo-port/src/components/Gallery/index.js
+++ photo-port/src/components/Gallery/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Project from '../Project';
-import image1 from '../../assets/small/commercial/sport-runner-on-start-260nw-567905164.jpeg';
+import image1 from '../../assets/small/commercial/1.jpg';
 import image2 from '../../assets/small/portraits/3.jpg';
 import image3 from '../../assets/small/food/1.jpg';
 import image4 from '../../assets/small/landscape/4.jpg';
 
-function Gallery() {
+function Gallery(props) {
     return (
         <section className="my-5">
 
-           <h2>My Projects</h2>
-           <Project image= {image1} title={'Run Buddy'} link={'https://melwel74.github.io/Run-Buddy-4.0/'}></Project>
-           <Project image={image2} title={'React Tion'} link={'https://github.com/melwel74/React-Tion.git'}></Project>
+           <h2>Projects</h2>
+           <Project image= {image1} title={'Run Buddy'} link={'https://github.com/melwel74/run-buddy5.git'}></Project>
+           <Project image={image2} title={'React-Tion'} link={'https://github.com/melwel74/React-Tion.git'}></Project>
            <Project image= {image3} title={'Taco-Loca-Locator'} link={'https://jamestw13.github.io/Taco-Loca-tor/'}></Project>
            <Project image={image4} title={'Social-Distance'} link={'https://melwel74.github.io/Social-Distance/'}></Project>
         </section>
--- photo-port/src/components/Header/index.js
+++ photo-port/src/components/Header/index.js
@@ -4,14 +4,17 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <nav>
-      <NavLink exact activeClassName="active" to="/">
-        Home
+      <NavLink exact activeClassName="active" to="/About-me">
+        About Me
       </NavLink>
-      <NavLink activeClassName="active" to="/users">
-        Users
+      <NavLink activeClassName="active" to="/Portfolio">
+        Portfolio
       </NavLink>
       <NavLink activeClassName="active" to="/contact">
         Contact
+      </NavLink>
+      <NavLink  activeClassName="active" to="/Resume">
+        Resume
       </NavLink>
     </nav>
   );
